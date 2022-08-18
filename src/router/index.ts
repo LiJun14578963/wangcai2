5e3ccea6fde17fff7f9ed834dbc4a9c22d8b8aa5
lijun <1094453726@qq.com>
2022-08-18 10:26:48 +0800

    创建三个页面â

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,34 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Home from '@/views/Home.vue';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
+  //我有这3个页面，默认页面跳转到money
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/money'//我要跳转到/money路径
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/labels',  //指定路径
+    component: Labels  //指定内容，但是并没有告诉在那个位置显示
+  },
+  {
+    path: '/statistics',
+    component: Statistics
+  },
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
