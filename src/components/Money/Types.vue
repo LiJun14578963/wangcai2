a84cbccff7f864c1e6eb04f5f1bbfd9032bb5689
lijun <1094453726@qq.com>
2022-08-20 15:08:20 +0800

    完成 Types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -13,44 +13,17 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-//必须从vue-property-decorator引入一个{Component}装饰器，然后吧修饰器修饰到class上面，class可以声明data 生命周期等等
+import {Component, Prop} from 'vue-property-decorator';
 @Component
 export default class Types extends Vue {
   type = '-';
-
-  selectType(type: string) {//type 只能是'-'和‘+’中的一个
+  selectType(type: string) {
     if (type !== '-' && type !== '+') {
       throw new Error('type is unknown');
     }
     this.type = type;
   }
-
-  createb() {}
-
-  mounted() {}
-
 }
-// export default {
-//   name: 'Types',
-//   props:['xxx'],
-//   data() {
-//     return {
-//       type: "-",//'-'表示支出，'+'表示收入
-//     }
-//   },
-//   mounted() {
-//     console.log(this.xxx)
-//   },
-//   methods: {
-//     selectType(type) {//type 只能是'-'和‘+’中的一个
-//       if (type !== '-' && type !== '+') {
-//         throw new Error('type is unknown')
-//       }
-//       this.type = type
-//     }
-//   }
-// };
 </script>
 
 <style lang="scss" scoped>
